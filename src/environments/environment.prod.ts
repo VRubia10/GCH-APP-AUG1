e1391b0d5bb88485f5143741ac59afee4f7bca4c
Vince Rubia <93848296+VRubia10@users.noreply.github.com>
2022-08-01 20:12:11 +0800

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAlbo0TE2brCRtQkKoojfnhnaSEFMFMGqg",
+    authDomain: "gch-app-51ca1.firebaseapp.com",
+    projectId: "gch-app-51ca1",
+    storageBucket: "gch-app-51ca1.appspot.com",
+    messagingSenderId: "254769020391",
+    appId: "1:254769020391:web:8689204659b5ac85cb1142"
+  },
+};
